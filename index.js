191c712fb921c491ec490b90a9eee20cf1580dd4
RCOM363 <rahullankeppanavar363@gmail.com>
2025-05-06 11:05:47 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const people = [
+    { age: 25, name: "John", id: 1 },
+    { age: 30, name: "Alice", id: 2 },
+    { age: 22, name: "Bob", id: 3 },
+    { age: 35, name: "Emily", id: 4 },
+  ];
+  
+const lastnames = [
+    { id: 1, lastname: "Patel" },
+    { id: 2, lastname: "Sinha" },
+    { id: 3, lastname: "Paul" },
+    { id: 4, lastname: "Bachan" },
+];
