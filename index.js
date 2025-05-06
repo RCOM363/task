8ca00baa1c30aa1ed2ed76b31802206371e012c8
RCOM363 <rahullankeppanavar363@gmail.com>
2025-05-06 11:14:07 +0000

    feat: update

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const people = [
-    { age: 25, name: "John", id: 1 },
-    { age: 30, name: "Alice", id: 2 },
-    { age: 22, name: "Bob", id: 3 },
-    { age: 35, name: "Emily", id: 4 },
-  ];
-  
-const lastnames = [
-    { id: 1, lastname: "Patel" },
-    { id: 2, lastname: "Sinha" },
-    { id: 3, lastname: "Paul" },
-    { id: 4, lastname: "Bachan" },
+  { age: 25, name: "John", id: 1 },
+  { age: 30, name: "Alice", id: 2 },
+  { age: 22, name: "Bob", id: 3 },
+  { age: 35, name: "Emily", id: 4 },
 ];
+
+const lastnames = [
+  { id: 1, lastname: "Patel" },
+  { id: 2, lastname: "Sinha" },
+  { id: 3, lastname: "Paul" },
+  { id: 4, lastname: "Bachan" },
+];
+
+const users = people.map((user) => {
+  const lname = lastnames.filter((ln) => ln.id === user.id);
+  const { lastname } = lname[0];
+  return {
+    ...user,
+    lastname,
+  };
+});
+
+console.log(users);
+
+// const sortedUsers = users
